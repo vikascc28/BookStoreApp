a60c528470ebf77bec98f293cf19efbe041c1a43
Vikas Chaurasia <vikascc28@gmail.com>
2024-06-18 14:28:59 +0530

    initial commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from './Components/Navbar'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
